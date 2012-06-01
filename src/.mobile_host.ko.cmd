a93e2b76516f3e7d313cfca5f7703b2582e437c7
Spencer <spencersevilla@gmail.com>
2012-06-01 15:54:04 -0700

    cleaning up directory

--- src/.mobile_host.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/spencer/Desktop/mnet/src/mobile_host.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.2.0-24-generic-pae/scripts/module-common.lds --build-id  -o /home/spencer/Desktop/mnet/src/mobile_host.ko /home/spencer/Desktop/mnet/src/mobile_host.o /home/spencer/Desktop/mnet/src/mobile_host.mod.o
