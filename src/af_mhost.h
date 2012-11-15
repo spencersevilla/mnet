701dd6e458ea6d11cee4a53ae1fc2f7d5f7284b1
Spencer <spencersevilla@gmail.com>
2012-11-15 11:27:24 -0800

    moving af_mhost functions into mhost_funcs
    for struct compilation!

--- src/af_mhost.h
+++ src/af_mhost.h
@@ -6,17 +6,4 @@
 #include "mhost_structs.h"
 #include "mhost_funcs.h"
 
-/* functions defined in af_mhost.c */
-int mhost_create(struct net *net, struct socket *sock, int protocol, int kern);
-int mhost_release(struct socket *sock);
-int mhost_bind(struct socket *sock, struct sockaddr *sa, int addr_len);
-int mhost_dgram_connect(struct socket *sock, struct sockaddr * uaddr, int addr_len, int flags);
-int mhost_getname(struct socket *sock, struct sockaddr *uaddr, int *uaddr_len, int peer);
-int mhost_ioctl(struct socket *sock, unsigned int cmd, unsigned long arg);
-int mhost_shutdown(struct socket *sock, int how);
-int mhost_sendmsg(struct kiocb *iocb, struct socket *sock, struct msghdr *msg, size_t size);
-int mhost_recvmsg(struct kiocb *iocb, struct socket *sock, struct msghdr *msg, size_t size, int flags);
-ssize_t mhost_sendpage(struct socket *sock, struct page *page, int offset, size_t size, int flags);
-int mhost_autobind(struct socket *sock);
-struct sk_buff *mhost_make_skb(struct sock *sk, void *from, int length, int hdrlen, unsigned int flags);
 #endif /* _AF_MHOST_H */
--- src/mhost_funcs.h
+++ src/mhost_funcs.h
@@ -7,16 +7,28 @@
 #include "mhost_defs.h"
 #include "mhost_structs.h"
 
+/* functions defined in af_mhost.c */
+int mhost_create(struct net *net, struct socket *sock, int protocol, int kern);
+int mhost_release(struct socket *sock);
+int mhost_bind(struct socket *sock, struct sockaddr *sa, int addr_len);
+int mhost_dgram_connect(struct socket *sock, struct sockaddr * uaddr, int addr_len, int flags);
+int mhost_getname(struct socket *sock, struct sockaddr *uaddr, int *uaddr_len, int peer);
+int mhost_ioctl(struct socket *sock, unsigned int cmd, unsigned long arg);
+int mhost_shutdown(struct socket *sock, int how);
+int mhost_sendmsg(struct kiocb *iocb, struct socket *sock, struct msghdr *msg, size_t size);
+int mhost_recvmsg(struct kiocb *iocb, struct socket *sock, struct msghdr *msg, size_t size, int flags);
+ssize_t mhost_sendpage(struct socket *sock, struct page *page, int offset, size_t size, int flags);
+int mhost_autobind(struct socket *sock);
+struct sk_buff *mhost_make_skb(struct sock *sk, void *from, int length, int hdrlen, unsigned int flags);
+int mhost_rcv_saddr_equal(const struct sock *sk1, const struct sock *sk2);
+
+/* defined in udp_mhost.c */
+int udp_mhost_rcv(struct sk_buff *skb);
+
 /* defined in mhost_table.c */
 int mhost_translate_sa(struct sockaddr *sa, struct sock *sk);
 int mhost_table_register(struct mhost_proto *proto);
 int insert_sockaddr_id(struct sockaddr *sa, short id);
 struct mhost_proto * mhost_proto_for_family(short family);
-
-/* defined in af_mhost.c */
-int mhost_rcv_saddr_equal(const struct sock *sk1, const struct sock *sk2);
-
-/* defined in udp_mhost.c */
-int udp_mhost_rcv(struct sk_buff *skb);
 
 #endif
