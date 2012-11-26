c9b11e7bcfc542fb3f9af7279ff45b25e1c11416
Spencer Sevilla <spencersevilla@gmail.com>
2012-11-26 13:17:22 -0800

    Adding IPv6 example and testing functions, cleaned up INET server

--- tests/make_tests.sh
+++ tests/make_tests.sh
@@ -2,8 +2,10 @@
 
 rm *.out
 gcc -o inet_client.out inet_client.c
+gcc -o inet6_client.out inet6_client.c
 gcc -o mhost_client.out mhost_client.c
 gcc -o inet_server.out inet_server.c
+gcc -o inet6_server.out inet6_server.c
 gcc -o mhost_server.out mhost_server.c
 gcc -o mhost_send.out mhost_send.c
 gcc -o mhost_loopback.out mhost_loopback_protos.c
