4adc25e6553488a146de769b241c9d89993f6649
Spencer <spencersevilla@gmail.com>
2013-04-25 17:45:43 -0700

    Improved Makefile to move all object files etc. into /build/

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 cd src
 make
 sudo insmod mobile_host.ko
+
+mv *.o ../build/
+mv *.ko ../build/
+mv modules.order ../build/
+mv Module.symvers ../build/
+mv mobile_host.mod.c ../build/
+
 cd ../tests
 ./make_tests.sh
 
