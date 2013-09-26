8ccf2f2f9a1a9f988ad10349ba611c589407d8a3
Spencer Sevilla <ssevilla@eduroam-226-241.ucsc.edu>
2013-09-26 14:45:07 -0700

    quick localhost test...

--- src/contable.c
+++ src/contable.c
@@ -4,6 +4,10 @@ unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb,
 			const struct net_device *in, const struct net_device *out,
 			int (*okfn)(struct sk_buff *))
 {
-	printk(KERN_INFO "NETFILTER!!!\n");
+
+	struct iphdr *iph = ip_hdr(skb);
+	if (iph.daddr == 0x0100007F) {
+		printk(KERN_INFO "NETFILTER!!!\n");
+	}
 	return NF_ACCEPT;
 }
